39b4af5ff01abd595f0166ebc6e066065a7d73c7
Andrew Sanchez <inbox.asanchez@gmail.com>
2018-11-27 09:54:08 -0700

    No evil highlight persist

--- init.el
+++ init.el
@@ -132,6 +132,7 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+  (setq evil-search-highlight-persist  nil)
   )
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
